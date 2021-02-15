9801e69f867c25c6b4607e94b7da7f1ae0c8ae29
adrita <nandamit2006@yahoo.com>
2021-02-15 15:42:35 +0530

    constraint created

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -36,6 +37,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    clog = new Log(200, 300, 75, PI/2);
+
+    chain = new Chain(clog.body, bird.body);
+
 }
 
 function draw(){
@@ -61,4 +66,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    clog.display();
+    chain.display();
 }
